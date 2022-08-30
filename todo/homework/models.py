064433027baa93da6b0b482eadbadc7f58d76998
Arzygul <arzyshmakenova@gmail.com>
2022-08-31 00:25:25 +0600

    created ToMeet model at homework app and add the fictitious datas to database

--- todo/homework/models.py
+++ todo/homework/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+#Напишите тип данных каждого из перечисленных типов моделей django.
+# models.BooleanField() булеан
+# models.DateTimeField() строка
+# models.FloatField() число
+# models.CharField() строка
+# models.EmailField() строка
+# models.TextField() строка
+
+class ToMeet(models.Model):
+    persone = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    date_of_meeting = models.DateTimeField()
+    comment = models.TextField(null=True, blank=True) # чтобы не всегда заполнять 
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
